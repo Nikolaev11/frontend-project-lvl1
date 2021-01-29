57cc127818b25ec9209f2dfef57aac117052d31a
Nikolaev <panikolaev@mail.ru>
2021-01-29 23:00:57 +0700

    after check 3 autotest test 2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 const roundsCount = 3;
 
 const playGame = (description, getGameData) => {
-  console.log(`Let's try again, Tirion!`);
+  console.log('Let\'s try again, Tirion!');
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
